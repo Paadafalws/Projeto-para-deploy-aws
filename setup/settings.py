af03ecbd77a78de5590eebe2d371b89ee07d3a67
Paadafalws <99057639+Paadafalws@users.noreply.github.com>
2023-07-21 09:39:13 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -127,3 +127,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
 }
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
