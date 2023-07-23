b6710ea6750592b3578b4edc9be8b67116cb97e8
Paadafalws <99057639+Paadafalws@users.noreply.github.com>
2023-07-23 01:55:28 -0300

    Update urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from clientes.views import ClientesViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register('clientes', ClientesViewSet)
